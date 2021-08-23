26d94fb6596bf4af26deaa322e2a352c757eb758
joao96 <jvpoletti@gmail.com>
2021-08-23 15:05:23 -0300

    Salvando dados do evento

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -32,6 +33,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const secret = req.headers['stripe-signature']
 
     let event: Stripe.Event;
+
     try {
       event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
     } catch (err) {
@@ -40,11 +42,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const { type } = event;
 
-    if (!relevantEvents.has(type)) {
-      console.log('Evento recebido', event)
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+            )
+
+            break;
+          default: 
+            throw new Error('Unhandled event')
+        }
+      }
+      catch (err) {
+        return res.json({ error: 'Webhook handler failed.'})
+      }
     }
 
-    res.json({ received: true})
+    res.json({ received: true })
   }
   else {
     res.setHeader('Allow', 'POST')
