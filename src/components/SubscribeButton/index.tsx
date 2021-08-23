b9540254f4810c0f48fb212dbbfb2b309d741d22
joao96 <jvpoletti@gmail.com>
2021-08-23 13:20:15 -0300

    Gerando sessão de checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,32 @@
+import { signIn, useSession } from "next-auth/client";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+// getServerSideProps (SSR)
+// getStaticProps (SSG)
+// API routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    // criação da checkout session
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
