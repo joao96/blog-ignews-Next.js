0b25617509fbddc6caae42292724b568dddcce65
joao96 <jvpoletti@gmail.com>
2021-09-11 16:56:08 -0300

    Testando página do post

---
+++ src/tests/pages/Post.spec.tsx
@@ -0,0 +1,90 @@
+import { render, screen } from "@testing-library/react";
+import { mocked } from "ts-jest/utils";
+import Post, { getServerSideProps } from "../../pages/posts/[slug]";
+import { getSession } from "next-auth/client";
+import { getPrismicClient } from "../../services/prismic";
+
+const post = {
+  slug: "my-new-post",
+  title: "My new Post",
+  content: "<p>Post content</p>",
+  updatedAt: "10 de Abril",
+};
+
+jest.mock("next-auth/client");
+jest.mock("../../services/prismic");
+
+describe("Post page", () => {
+  it("renders correctly", () => {
+    render(<Post post={post} />);
+
+    expect(screen.getByText("My new Post")).toBeInTheDocument();
+    expect(screen.getByText("Post content")).toBeInTheDocument();
+  });
+
+  it("redirects user if no subscription is found", async () => {
+    const getSessionMocked = mocked(getSession);
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: null,
+    } as any);
+
+    const response = await getServerSideProps({
+      params: { slug: "my-new-post" },
+    } as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        redirect: expect.objectContaining({
+          destination: "/posts/preview/my-new-post",
+        }),
+      })
+    );
+  });
+
+  it("loads initial data", async () => {
+    const getSessionMocked = mocked(getSession);
+
+    const getPrismicClientMocked = mocked(getPrismicClient);
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [
+            {
+              type: "heading",
+              text: "My new post",
+            },
+          ],
+          content: [
+            {
+              type: "paragraph",
+              text: "Post content",
+            },
+          ],
+        },
+        last_publication_date: "04-01-2021",
+      }),
+    } as any);
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: "fake-active-subscription",
+    } as any);
+
+    const response = await getServerSideProps({
+      params: { slug: "my-new-post" },
+    } as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: "my-new-post",
+            title: "My new post",
+            content: "<p>Post content</p>",
+            updatedAt: "01 de abril de 2021",
+          },
+        },
+      })
+    );
+  });
+});
